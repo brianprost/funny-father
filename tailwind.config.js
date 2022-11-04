6d1e30063c32ca2e451447e7be3676f795ff86e4
Brian Prost <brian.s.prost@gmail.com>
2022-11-04 00:09:58 -0400

    new custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
+  daisyui: {
+    themes: [
+      {
+        dj: {
+          primary: "#b29e84",
+          secondary: "#7c898b",
+          accent: "#423e3a",
+          neutral: "#253439",
+          "base-100": "#f6f4f1",
+          info: "#7c898b",
+          success: "#253932",
+          warning: "#eab308",
+          error: "#7c2d12",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {
       fontFamily: {
