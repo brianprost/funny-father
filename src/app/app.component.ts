54b8d9c70695c21faaa8cae0126e967372bac0ee
Brian Prost <brian.s.prost@gmail.com>
2022-11-04 00:34:15 -0400

    changed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,5 +18,5 @@ export class AppComponent {
   // this class is the landing page for the app, and will just display a random dad joke from the realtime database
   // the joke will be displayed in a card, and the card will have a button to get a new joke
   // the card will also have a button to add the joke to the user's favorites
-  title = 'ng-dad-jokes';
+  title = 'Funny Father';
 }
