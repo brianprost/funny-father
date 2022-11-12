1a2484e2af97d48c24f840d5ddbd87ee462010ce
Brian Prost <brian.s.prost@gmail.com>
2022-11-12 18:30:41 -0500

    Aws migration (#5)
    * removed the ability for non-Apple users to add jokes
    
    * temporarily disabling account and joke list pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,19 +9,23 @@ import { JokesComponent } from './jokes/jokes.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'jokes', component: JokesComponent },
+  // { path: 'jokes', component: JokesComponent },
   // account, and then account/login, account/signupaccount/info
+  // {
+  //   path: 'account',
+  //   component: AccountComponent,
+  // },
+  // {
+  //   path: 'account/login',
+  //   component: LoginComponent,
+  // },
+  // {
+  //   path: 'account/signup',
+  //   component: SignupComponent,
+  // },
   {
-    path: 'account',
-    component: AccountComponent,
-  },
-  {
-    path: 'account/login',
-    component: LoginComponent,
-  },
-  {
-    path: 'account/signup',
-    component: SignupComponent,
+    path: 'jokes/add',
+    component: AddNewJokeComponent,
   },
   {
     path: 'jokes/add',
--- src/app/jokes/add-new-joke/add-new-joke.component.ts
+++ src/app/jokes/add-new-joke/add-new-joke.component.ts
@@ -9,34 +9,54 @@ import IJoke from 'src/app/types/IJoke';
   template: `
     <div class="flex flex-col justify-center items-center h-[80vh]">
       <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <h2 class="card-title">Add new joke</h2>
-          <form [formGroup]="newJokeFormGroup" (ngSubmit)="onSubmit()">
-            <input
-              type="text"
-              id="setup"
-              placeholder="Setup"
-              class="input input-bordered w-full my-2"
-              formControlName="setup"
-            />
-            <input
-              type="text"
-              id="punchline"
-              placeholder="Punchline"
-              class="input input-bordered w-full my-2"
-              formControlName="punchline"
-            />
-            <div class="card-actions justify-end mt-4">
-              <button type="submit" class="btn btn-primary">Add joke</button>
-            </div>
-          </form>
-        </div>
+        <ng-container *ngIf="isAppleDevice(); else getAMac">
+          <div class="card-body">
+            <h2 class="card-title">Add new joke</h2>
+            <form [formGroup]="newJokeFormGroup" (ngSubmit)="onSubmit()">
+              <input
+                type="text"
+                id="setup"
+                placeholder="Setup"
+                class="input input-bordered w-full my-2"
+                formControlName="setup"
+              />
+              <input
+                type="text"
+                id="punchline"
+                placeholder="Punchline"
+                class="input input-bordered w-full my-2"
+                formControlName="punchline"
+              />
+              <div class="card-actions justify-end mt-4">
+                <button type="submit" class="btn btn-primary">Add joke</button>
+              </div>
+            </form>
+          </div>
+        </ng-container>
       </div>
     </div>
+
+    <ng-template #getAMac>
+      <!-- No longer need to add noreferrer noopener: https://chromestatus.com/feature/6140064063029248 -->
+      <a href="https://www.apple.com/mac/" target="_blank">
+        <p
+          class="flex flex-col justify-center items-center text-center link h-40 text-4xl mx-12"
+        >
+          Buy a Mac to add new jokes.
+        </p>
+      </a>
+    </ng-template>
   `,
   styles: [],
 })
 export class AddNewJokeComponent implements OnInit {
+  isAppleDevice(): boolean {
+    // returns whether the device is an apple device, including iOS, iPadOS, macOS, watchOS, and tvOS
+    const appleDevices = ['iPhone', 'iPad', 'iPod', 'Macintosh', 'Watch', 'TV'];
+    return appleDevices.some((device) =>
+      window.navigator.userAgent.includes(device)
+    );
+  }
   newJokeFormGroup = new FormGroup({
     setup: new FormControl('', { nonNullable: true }),
     punchline: new FormControl('', { nonNullable: true }),
