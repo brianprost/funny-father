78c7219b79f07efbe41995447448a1301740c46c
Brian Prost <brian.s.prost@gmail.com>
2022-11-12 18:22:47 -0500

    temporarily disabling account and joke list pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,20 +9,20 @@ import { JokesComponent } from './jokes/jokes.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'jokes', component: JokesComponent },
+  // { path: 'jokes', component: JokesComponent },
   // account, and then account/login, account/signupaccount/info
-  {
-    path: 'account',
-    component: AccountComponent,
-  },
-  {
-    path: 'account/login',
-    component: LoginComponent,
-  },
-  {
-    path: 'account/signup',
-    component: SignupComponent,
-  },
+  // {
+  //   path: 'account',
+  //   component: AccountComponent,
+  // },
+  // {
+  //   path: 'account/login',
+  //   component: LoginComponent,
+  // },
+  // {
+  //   path: 'account/signup',
+  //   component: SignupComponent,
+  // },
   {
     path: 'jokes/add',
     component: AddNewJokeComponent,
