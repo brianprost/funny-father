65acf4b0f45d97afd16945c57152e1acdebdef45
Brian Prost <brian.s.prost@gmail.com>
2022-11-12 18:46:39 +0000

    put environment variables in prod env as well

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true,
+  // i know putting these here is bad, but it's only temporary
+  // will remove after settling on hosting
+  AWS_ACCESS_KEY_ID: 'AKIAZ2GNH2ABJQNFFJ66',
+  AWS_SECRET_ACCESS_KEY: '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',
+  AWS_REGION: 'us-east-1',
 };
