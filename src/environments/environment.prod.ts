a41827d7b38364d0f30957df5a826ca87c2ab029
Brian Prost <brian.s.prost@gmail.com>
2022-11-14 16:25:58 -0500

    put them brackets back in...we needed them!

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  AWS_ACCESS_KEY_ID: process.env.BEZOAWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY: process.env.BEZOAWS_SECRET_ACCESS_KEY,
-  AWS_REGION: process.env.BEZOAWS_REGION,
+  AWS_ACCESS_KEY_ID: process.env['BEZOAWS_ACCESS_KEY_ID'],
+  AWS_SECRET_ACCESS_KEY: process.env['BEZOAWS_SECRET_ACCESS_KEY'],
+  AWS_REGION: process.env['BEZOAWS_REGION'],
 };
