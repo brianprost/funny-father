05416d6575b1c8449de015fcac8eabc9bf2c2df5
Brian Prost <bprost@humrro.org>
2022-11-23 04:08:46 +0000

    Squashed commit of the following
    commit b1c77bf0 
    Author: Brian Prost <brian.s.prost@gmail.com> 
    Date: Tue Nov 22 2022 23:06:29 GMT-0500 (Eastern Standard Time) 
    
        added some padding on updated footer for mobile
    
    
    commit f62afed6 
    Author: Brian Prost <brian.s.prost@gmail.com> 
    Date: Tue Nov 22 2022 23:00:56 GMT-0500 (Eastern Standard Time) 
    
        added DMCA complaint to footer
    
    added "initially" to the footer in the hopes that others try messing around with this project as well :)
    
    ...goal is to eventually take my name off of it and maybe put the team's name on it!

--- src/app/footer/made-by-brian/made-by-brian.component.ts
+++ src/app/footer/made-by-brian/made-by-brian.component.ts
@@ -4,19 +4,26 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-made-by-brian',
   template: `
     <div
-      class="text-md flex h-16 items-center justify-end bg-skin text-skin mx-4"
+      class="text-md flex h-16 items-center flex-col lg:flex-row gap-4 lg:gap-0 lg:justify-between bg-skin text-skin mx-4 text-secondary-content mb-6 lg:mb-0"
     >
-      <p class="text-secondary-content">
-        made by:
+      <a
+        href="https://brianprost.com/#contact"
+        target="_blank"
+        class="hover:text-secondary-focus hover:underline hover:font-bold"
+      >
+        Submit DMCA Complaint
+      </a>
+      <div>
+        initially made by:
         <a
           href="https://brianprost.com/"
           target="_blank"
           rel="noopener noreferrer"
-          class="rounded-xl border-2 border-accent-content py-2 px-3 font-title text-2xl text-accent hover:bg-primary-focus hover:text-base-100"
+          class="rounded-xl border-2 border-accent-content py-2 px-3 font-title text-2xl text-accent hover:bg-accent hover:text-base-100"
         >
           bRIAN pRōST
         </a>
-      </p>
+      </div>
     </div>
   `,
   styles: [],
