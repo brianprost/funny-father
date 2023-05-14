a8d0e70d2b16f199892e5c1b75792104136585e7
Brian Prost <brian.s.prost@gmail.com>
2023-05-14 13:45:02 -0400

    got the account info component to load the data properly

--- src/app/account/account-info/account-info.component.ts
+++ src/app/account/account-info/account-info.component.ts
@@ -1,40 +1,60 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { Auth, user } from '@angular/fire/auth';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { FunnyFatherUser } from 'src/app/models/FunnyFatherUser';
-import { UserService } from 'src/app/services/user.service';
+import { Component, inject } from '@angular/core';
+import { Auth, UserInfo, user } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-account-info',
   template: `
     <div class="flex h-screen justify-center items-center">
-      <div class="card w-96 bg-base-100 shadow-xl" *ngIf="userProfile$ | async as userProfile">
-        <figure class="px-10 pt-10">
+      <div
+        class="card w-96 bg-base-100 shadow-xl"
+        *ngIf="userInfo$ | async as userInfo"
+      >
+        <figure class="px-10 pt-10" *ngIf>
           <app-avatar></app-avatar>
         </figure>
         <div class="card-body items-center text-center">
-          <h2 class="card-title">{{userProfile.firstName}} {{ userProfile.lastName }}</h2>
-          <ng-container *ngFor="let attribute of [userProfile.email, userProfile.phone]">
-            <p>{{ attribute }}</p>
-          <!-- <input
-            type="email"
-            placeholder="{{ userProfile.email }}"
-            class="input input-bordered w-full max-w-xs"
-          />
-          <input
-            type="tel"
-            placeholder="{{ userProfile.phone }}"
-            class="input input-bordered w-full max-w-xs"
-          />
-          <input
-            type="password"
-            placeholder="*************"
-            class="input input-bordered w-full max-w-xs"
-          />
-          <div class="card-actions">
-            <button class="btn btn-primary">Update account</button>
-          </div> -->
+          <ng-container *ngFor="let attribute of userInfo | keyvalue">
+            <!-- filter out anything that isn't displayName, email, phoneNumber, or photoUrl -->
+            <ng-container
+              *ngIf="
+                attribute.key === 'displayName' ||
+                attribute.key === 'email' ||
+                attribute.key === 'phoneNumber'
+              "
+            >
+              <h2 class="card-title" *ngIf="attribute.key === 'displayName'">
+                {{ attribute.value ?? 'nameless person' }}
+              </h2>
+              <input
+                *ngIf="attribute.key == 'displayName'"
+                type="text"
+                placeholder="{{ attribute.value ?? 'Your name' }}"
+                class="input input-bordered w-full max-w-xs"
+              />
+              <input
+                *ngIf="attribute.key == 'email'"
+                type="email"
+                placeholder="{{ attribute.value }}"
+                class="input input-bordered w-full max-w-xs"
+              />
+              <input
+                *ngIf="attribute.key == 'phoneNumber'"
+                type="tel"
+                placeholder="{{ attribute.value }}"
+                class="input input-bordered w-full max-w-xs"
+              />
+              <!-- <input
+                type="password"
+                placeholder="*************"
+                class="input input-bordered w-full max-w-xs"
+              /> -->
+            </ng-container>
           </ng-container>
+          <div class="card-actions">
+            <!-- make it disabled if the form hasn't been touched -->
+            <button class="btn btn-primary">Update account</button>
+          </div>
         </div>
       </div>
     </div>
@@ -42,16 +62,10 @@ import { UserService } from 'src/app/services/user.service';
   styles: [],
 })
 export class AccountInfoComponent {
-  userService = inject(UserService);
-  userProfile$: Observable<FunnyFatherUser | null> = this.userService.userProfile;
+  private auth = inject(Auth);
+  userInfo$: Observable<UserInfo | null> = user(this.auth);
 
   updateUserProfile() {
-    // do update user profile
-  }
-
-  constructor() {
-    this.userProfile$.subscribe((user) => {
-      console.log(user);
-    });
+    // update any or all fields of the user's profile
   }
 }
